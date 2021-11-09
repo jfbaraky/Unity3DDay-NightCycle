eee3e0bc8e9ec457f4038dc1f65344ad227614bc
JpBaraky <jp.baraky@gmail.com>
2021-11-09 17:02:44 -0300

    Light Intensity
    -Now the Light increases and decreases with the cycle

--- Assets/Scripts/skyDomeDayNight.cs
+++ Assets/Scripts/skyDomeDayNight.cs
@@ -2,19 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class skyDomeDayNight : MonoBehaviour
 {
     private Material material;
+    
     [Header("Sky Assets Settings")] //Set the moon, starts and sun objects
     public GameObject Moon;
     public GameObject Stars;
     public GameObject Sun;
+    [Header("Light Settings")] //Set the moon, starts and sun objects
+
+    public Light Light;
+    private float maxLight = 1.0f;
+    private float minLight = 0.0f;
+    static float t;
     [Header("Sky Movement Systems")]
     public float cycleSpeed; //Set how fast the night and day cycle will be by changing the offSet
      private float offSet; //Set how the Cycle will start
     void Start()
     {
         material = GetComponent<Renderer>().material; // Gets the material of the Sky
+    
     }
 
   
@@ -22,6 +31,7 @@ public class skyDomeDayNight : MonoBehaviour
     {
        CycleDay();
        MoonAndSun();
+       SunLight();
 
     }
     void CycleDay(){ // Day and Night Cycle
@@ -30,18 +40,35 @@ public class skyDomeDayNight : MonoBehaviour
         if(offSet >= 1){
             offSet = 0;
         }
+       
     }
     void MoonAndSun(){ 
         if(offSet >= 0.2 && offSet <= 0.7){
-            Moon.SetActive(true);
-            Stars.SetActive(true);
-            Sun.SetActive(false);
-        } 
-        else{
-             Moon.SetActive(false);
+            Moon.SetActive(false);
             Stars.SetActive(false);
             Sun.SetActive(true);
+        } 
+        else{
+             Moon.SetActive(true);
+            Stars.SetActive(true);
+            Sun.SetActive(false);
         }
+
+
+    }   
+    void SunLight(){ //Increase and Decrease the intensity of the light with the cycle
+
+        
+        Light.intensity = Mathf.Lerp(minLight, maxLight, t);
+        t += cycleSpeed * Time.deltaTime * 2;
+        if (t > 1.0f)
+        {
+            float temp = maxLight;
+            maxLight = minLight;
+            minLight = temp;
+            t = 0.0f;
+        }
+
 
     }
 }
